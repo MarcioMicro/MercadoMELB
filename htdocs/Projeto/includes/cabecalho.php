c6cf85b56b339e51a44e3539beeef177356b4434
EduHenriqs <110845651+EduHenriqs@users.noreply.github.com>
2023-03-31 21:38:02 -0300

    Add files via upload

--- htdocs/Projeto/includes/cabecalho.php
+++ htdocs/Projeto/includes/cabecalho.php
@@ -2,7 +2,7 @@
 <link rel="stylesheet" href="css/bootstrap.min.css">
 <link rel="stylesheet" href="css/bs-admin.css">
 <link rel="stylesheet" href="css/style.css">
-
+<script src="js/Chart.min.js"></script>
 <script src="js/bootstrap.bundle.min.js"></script>
 <script src="includes/sb-admin/js/sb-admin-2.min.js"></script>
 
@@ -26,7 +26,7 @@
 <body class="sb-nav-fixed">
   <nav class="sb-topnav navbar navbar-expand navbar-azul">
 
-    <a class="navbar-brand ps-3" href="#">Supermercado MELB</a>
+    <a class="navbar-brand ps-3" href="projeto_inicio.php"><img src="images/logo_melb.png" class="login-logo"></a>
 
     <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i> </button>
 <div class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0"></div>
@@ -49,10 +49,14 @@
       <nav class="sb-sidenav accordion sidenav-azul" id="sidenavAccordion">
         <div class="sb-sidenav-menu">
           <div class="nav">
-            <div class="sb-sidenav-menu-heading">Core</div>
-            <a class="nav-link" href="index.html">
-              <div class="sb-nav-link-icon"> <i class="fas fa-tachometer-alt"></i></div>
-              Dashboard
+            <div class="sb-sidenav-menu-heading">Modulos</div>
+            <a class="nav-link" href="pag_cadastro_func.php">
+              <div class="sb-nav-link-icon"> <i class="fa-solid fa-address-card"></i></div>
+              Cadastrar funcionário
+            </a>
+            <a class="nav-link" href="pag_estoque.php">
+              <div class="sb-nav-link-icon"> <i class="fa-solid fa-boxes-stacked"></i></div>
+              Estoque
             </a>
             <div class="sb-sidenav-menu-heading">Interface</div>
             <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
@@ -66,37 +70,6 @@
                 <a class="nav-link" href="layout-sidenav-light.html">Light Sidenav</a>
               </nav>
             </div>
-            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
-              <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i> </div>
-              Pages
-              <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-            </a>
-            <div class="collapse" id="collapsePages" aria-labelledby="headingTwo" data-bs-parent="#sidenavAccordion">
-              <nav class="sb-sidenav-menu-nested nav accordion" id="sidenavAccordionPages">
-                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#pagesCollapseAuth" aria-expanded="false" aria-controls="pagesCollapseAuth">
-                  Authentication
-                  <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                </a>
-                <div class="collapse" id="pagesCollapseAuth" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordionPages">
-                  <nav class="sb-sidenav-menu-nested nav">
-                    <a class="nav-link" href="login.html">Login</a>
-                    <a class="nav-link" href="register.html">Register</a>
-                    <a class="nav-link" href="password.html">Forgot Password</a>
-                  </nav>
-                </div>
-                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#pagesCollapseError" aria-expanded="false" aria-controls="pagesCollapseError">
-                  Error
-                  <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                </a>
-                <div class="collapse" id="pagesCollapseError" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordionPages">
-                  <nav class="sb-sidenav-menu-nested nav">
-                    <a class="nav-link" href="401.html">401 Page</a>
-                    <a class="nav-link" href="404.html">404 Page</a>
-                    <a class="nav-link" href="500.html">500 Page</a>
-                  </nav>
-                </div>
-              </nav>
-            </div>
             <div class="sb-sidenav-menu-heading">Addons</div>
             <a class="nav-link" href="charts.html">
               <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
@@ -107,10 +80,6 @@
               Tables
             </a>
           </div>
-        </div>
-        <div class="sb-sidenav-footer">
-          <div class="small">Logged in as:</div>
-          <?php print ucfirst($_SESSION['usuario_nome']) ?>
         </div>
       </nav>
     </div>

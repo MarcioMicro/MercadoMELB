b79afe9e93004d6fd22db7ff239409e8b5498754
EduHenriqs <110845651+EduHenriqs@users.noreply.github.com>
2023-05-30 17:01:59 -0300

    Add files via upload

--- htdocs/Projeto/includes/cabecalho.php
+++ htdocs/Projeto/includes/cabecalho.php
@@ -20,9 +20,9 @@
 
 
 <?php
-//error_reporting(0);
-//ini_set('display_errors', 0);
-//ini_set('display_startup_errors', 0);
+error_reporting(0);
+ini_set('display_errors', 0);
+ini_set('display_startup_errors', 0);
 include "includes/conect.php";
 $id_func = $_SESSION['usuario_id'];
 
